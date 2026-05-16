f14b425d429f8a9a2d579fbf351ad2757c186168
s selva ganesh <selvaganesh.s9629@gmail.com>
2026-05-17 00:07:17 +0530

    coding plateform page updated again 3

--- src/components/coding_platforms.jsx
+++ src/components/coding_platforms.jsx
@@ -4,7 +4,17 @@ import "./index.css";
 export default function CodingPlatforms() {
 
   function frameLoaded() {
-    document.querySelector(".navbarhider").style.display = "block";
+    setTimeout(() => {
+        window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+          });
+
+        document.querySelector(".navbarhider").style.display = "block";
+
+        document.querySelector(".iframecode").style.display = "block";
+
+    }, 1200); // 1 second
 }
   return (
     <div className="coding_platform">
